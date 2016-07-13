ade3e923471a72ce4d82807ebbe2b8468c76608b
jamwhi <jamesgeewhite@gmail.com>
2016-07-13 20:14:24 +1000

    Fix absolute path to be relative. Fix ENOENT crashes. Add navigation links

--- requestHandlers.js
+++ requestHandlers.js
@@ -17,6 +17,8 @@ function start(response, postData) {
 		'<input type="file" name="upload">' +
 		'<input type="submit" value="Upload file" />' +
 		'</form>' +
+		'<br />' +
+		'<a href=\'show\'>Show current picture</a>' +
 		'</body>' +
 		'</html>';
 		
@@ -29,19 +31,28 @@ function upload(response, request) {
 	console.log("Request handler 'upload' was called.");
 	
 	var form = new formidable.IncomingForm();
-	form.uploadDir = "/Work/Node/Project 1/tmp";
+	form.uploadDir = "tmp";
+
+	// If 'tmp' folder doesn't exist, create it
+	if (!fs.existsSync("tmp")) {
+		fs.mkdirSync("tmp");
+	}
+
 	console.log("about to parse");
 	form.parse(request, function(error, fields, files) {
 		console.log("parsing done");
+
+		
 		
 		/* Possible error on Windows systems:
 			tried to rename to an already existing file */
-		fs.rename(files.upload.path, "/Work/Node/Project 1/tmp/test.png", function(error) {
+		fs.rename(files.upload.path, "tmp/test.png", function(error) {
 			if (error) {
-				fs.unlink("/Work/Node/Project 1/tmp/test.png");
-				fs.rename(files.upload.path, "/Work/Node/Project 1/tmp/test.png");
+				fs.unlink("tmp/test.png");
+				fs.rename(files.upload.path, "tmp/test.png");
 			}
 		});
+
 		response.writeHead(200, {"Content-Type": "text/html"});
 		response.write("received image: <br/>");
 		response.write("<img src='/show' />");
@@ -51,8 +62,39 @@ function upload(response, request) {
 
 function show(response) {
 	console.log("Request handler for 'show' was called.");
-	response.writeHead(200, {"Content-Type": "image/png"});
-	fs.createReadStream("/Work/Node/Project 1/tmp/test.png").pipe(response);
+
+	// Check if file exists
+	fs.stat("tmp/test.png", function(err, stat) {
+		if(err == null) {
+
+			console.log('File exists');
+			response.writeHead(200, {"Content-Type": "image/png"});
+			fs.createReadStream("tmp/test.png").pipe(response);
+
+		} else if(err.code == 'ENOENT') {
+
+			console.log('File does not exist');
+			var body = '<html>'+
+				'<head>'+
+				'<meta http-equiv="Content-Type" '+
+				'content="text/html; charset=UTF-8" />' +
+				'</head>' +
+				'<body>' +
+				'No picture here right now' +
+				'<br /><br />' +
+				'<a href=\'start\'>Back</a>' +
+				'</body>' +
+				'</html>';
+			
+			response.writeHead(200, {"Content-Type": "text/html"});
+			response.write(body);
+			response.end();
+
+		} else {
+			console.log('Some other error: ', err.code);
+		}
+	});
+
 }
 
 exports.start = start;
